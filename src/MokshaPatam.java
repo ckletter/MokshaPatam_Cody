63c52d33793037f9a389c676a0ac8de612b0ffe2
codykletter <cody.kletter@menloschool.org>
2024-09-04 10:33:52 -0700

    added recursive BFS search in progress

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -1,5 +1,7 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Moksha Patam
@@ -19,48 +21,44 @@ public class MokshaPatam {
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
         int currentSquare = 1;
-        int numRolls = 0;
-        // Calculate last square
         int lastSquare = boardsize * boardsize;
-        ArrayList<Integer> finalRolls = new ArrayList<Integer>();
-        tryLadder(lastSquare, ladders, snakes, currentSquare, numRolls, finalRolls);
-        // Calculate minimum roll of all possible combinations
-        int minRolls = Integer.MAX_VALUE;
-        for (int roll : finalRolls) {
-            System.out.println(roll);
-            if (roll < minRolls) {
-                minRolls = roll;
-            }
+        int ladderLength = ladders.length;
+        int snakesLength = snakes.length;
+        Queue<Integer> bfsQueue = new LinkedList<Integer>();
+        ArrayList<Integer> alreadyExplored = new ArrayList<Integer>();
+        // initialize new array lookup table to find starts of snakes and ladders
+        int[] snakeLadderStarts = new int[boardsize];
+        for (int i = 0; i < ladders.length; i++) {
+            snakeLadderStarts[ladders[i][0]] = ladders[i][1];
         }
-        return minRolls;
     }
-    public static void tryLadder(int lastSquare, int[][] ladders, int[][] snakes, int currentSquare, int numRolls, ArrayList<Integer> finalRolls) {
-        int numLadders = ladders.length;
-        boolean laddersLeft = false;
-        // Loop through each ladder on the board
-        for (int i = 0; i < numLadders; i++) {
-            int currLadder = ladders[i][0];
-            // If already past ladder, skip (can't try that ladder)
-            if (currentSquare > currLadder) {
-                continue;
+    public static int tryBFS(int lastSquare, int currentSquare, int numRolls, Queue<Integer> bfsQueue) {
+        // Base case - if lastSquare is reached, return number of rolls used
+        if (currentSquare == lastSquare) {
+            return numRolls;
+        }
+        for (int i = 1; i < 7; i++) {
+            if (isSnake()) {
+                // Do stuff
             }
-            // Calculate number of rolls from current square to get to ladder and add to numRolls
-            int rollsToLadder = (int) Math.ceil((currLadder - currentSquare) / 6.0);
+            else if (isLadder()) {
 
-            numRolls += rollsToLadder;
-            // Update current square once moved
-            currentSquare = ladders[i][1];
-            // Set laddersLeft equal to true (possible to move to another ladder)
-            laddersLeft = true;
-            tryLadder(lastSquare, ladders, snakes, currentSquare, numRolls, finalRolls);
+            }
+            // Add next possible roll to queue
+            bfsQueue.add(currentSquare + i);
         }
-        // If not possible to move to another ladder,
-        // calculates rolls to the end and adds final number of rolls to final rolls array list, then returns
-        if (!laddersLeft) {
-            int rollsToEnd = (int) Math.ceil((lastSquare - currentSquare) / 6.0);
-            numRolls += rollsToEnd;
-            finalRolls.add(numRolls);
-            return;
+        // If there are no nodes left in the queue, return -1 signaling we are in a loop
+        if (bfsQueue.peek() == null) {
+            return -1;
         }
+        // Increment rolls count by 1
+        numRolls += 1;
+        int nextNode = bfsQueue.remove();
+        return tryBFS(lastSquare, nextNode, numRolls, bfsQueue);
+    }
+    public static boolean isSnake() {
+    }
+    public static boolean isLadder() {
+
     }
 }
